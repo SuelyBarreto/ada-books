d4826650d6abaad45fc73e12076bb74675d6dc17
SuelyBarreto <suely@barreto.us>
2020-06-07 20:43:03 -0700

    User model and controller created, OAuth login completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
-
+  root 'books#index'
   # Routes that operate on the book collection
   get '/books', to: 'books#index', as: 'books'
   get '/books/new', to: 'books#new', as: 'new_book'
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
   patch '/books/:id', to: 'books#update'
   delete '/books/:id', to: 'books#destroy'
+
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create"
+  delete "/logout", to: "users#destroy", as: "logout"  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_174307) do
+ActiveRecord::Schema.define(version: 2020_06_08_025635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_04_22_174307) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "uid"
+    t.string "name"
+    t.string "email"
+    t.string "provider"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
